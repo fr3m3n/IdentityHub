a2b23761f2a16999e21d97f7005f74bd6eee59d5
fr3m3n <mtimochenko@tutanota.com>
2023-10-12 14:25:28 -0300

    data.sql file with some sample data for testing

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,14 @@
--- Insert roles
+-- Inserting roles
 INSERT INTO role (id, role_name) VALUES (1, 'ROLE_ADMIN');
 INSERT INTO role (id, role_name) VALUES (2, 'ROLE_USER');
 INSERT INTO role (id, role_name) VALUES (3, 'ROLE_GUEST');
 
--- Insert users with encrypted dummy passwords (you should replace these with real encrypted passwords)
-INSERT INTO usr (id, username, password, created_at, updated_at)
-VALUES (1, 'user1', '{bcrypt}$2a$10$VKJF0JUfnTPZxGEP6t9T0e4UzkevWD6h9I0gSMvOF58wLh1hrxfH2', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+-- Inserting users (Note: For simplicity, using plain passwords. In a real application, consider hashing the passwords.)
+INSERT INTO usr (id, username, password, created_at, updated_at) VALUES (1, 'admin', 'admin123', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO usr (id, username, password, created_at, updated_at) VALUES (2, 'user', 'user123', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO usr (id, username, password, created_at, updated_at) VALUES (3, 'guest', 'guest123', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
-INSERT INTO usr (id, username, password, created_at, updated_at)
-VALUES (2, 'user2', '{bcrypt}$2a$10$VKJF0JUfnTPZxGEP6t9T0e4UzkevWD6h9I0gSMvOF58wLh1hrxfH2', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
-INSERT INTO usr (id, username, password, created_at, updated_at)
-VALUES (3, 'user3', '{bcrypt}$2a$10$VKJF0JUfnTPZxGEP6t9T0e4UzkevWD6h9I0gSMvOF58wLh1hrxfH2', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
--- Associate roles with users
-INSERT INTO user_roles (id, usr_id, role_id) VALUES (1, 1, 1); -- user1 has ROLE_ADMIN
-INSERT INTO user_roles (id, usr_id, role_id) VALUES (2, 2, 2); -- user2 has ROLE_USER
-INSERT INTO user_roles (id, usr_id, role_id) VALUES (3, 3, 3); -- user3 has ROLE_GUEST
+-- Associating users with roles
+INSERT INTO user_roles (usr_id, role_id) VALUES (1, 1);  -- admin has ROLE_ADMIN
+INSERT INTO user_roles (usr_id, role_id) VALUES (2, 2);  -- user has ROLE_USER
+INSERT INTO user_roles (usr_id, role_id) VALUES (3, 3);  -- guest has ROLE_GUEST
